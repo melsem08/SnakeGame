9ad1fa491b9f5ef512e5652a62347aa88c5de123
Semen Melnykov <gersen18@gmail.com>
2024-02-07 20:00:51 +0000

    Spatial Logic Added

---
+++ SnakeGame/Position.cs
@@ -0,0 +1,20 @@
+﻿namespace SnakeGame
+{
+    public class Position
+    {
+        public int Row { get; }
+
+        public int Column { get; }
+
+        public Position(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Position Translate(Direction direction)
+        {
+            return new Position(Row + direction.RowOffset, Column + direction.ColumnOffset);
+        }
+    }
+}
